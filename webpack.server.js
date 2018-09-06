cdba1f9b825d0efd68cea06bcecb4d4a89a150f4
danielnmai <danielmai1728@gmail.com>
2018-09-05 21:11:55 -0700

    exclude node modules on server config

--- webpack.server.js
+++ webpack.server.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const merge = require('webpack-merge')
 const baseConfig = require('./webpack.base.js')
-
+const webpackNodeExternal = require('webpack-node-externals')
 const config  = {
     // Inform webpack that we're building a bundle 
     //for Nodejs, rather than for the browser
@@ -14,7 +14,9 @@ const config  = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'build')
-    }
+    },
+
+    externals: [webpackNodeExternal()]
 }
 
 module.exports = merge(baseConfig, config)
